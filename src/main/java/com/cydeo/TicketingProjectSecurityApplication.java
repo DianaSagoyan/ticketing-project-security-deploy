c80536b532855ab529244409872224eb9fc65aa4
DianaSagoyan <dianasagoyan@yahoo.com>
2023-01-20 11:52:47 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
@@ -0,0 +1,29 @@
+package com.cydeo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication  //this includes @Configuration
+public class TicketingProjectSecurityApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TicketingProjectSecurityApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper(){
+
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.cydeo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(BaseEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Boolean isDeleted = false;
+
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
+    private Long insertUserId;
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
+    private Long lastUpdateUserId;
+
+
+
+
+
+
+}
